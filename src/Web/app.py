63566e11e1816cbc51922cd0644faf68965a948f
yura97 <yura.iwanchev@yandex.ru>
2021-10-09 21:16:58 +0300

    show sizes

--- src/Web/app.py
+++ src/Web/app.py
@@ -11,6 +11,8 @@ from src.alghoritm import optimize
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '1234567'
 
+RESULT_NAME = dict()
+
 
 @app.route('/', methods=('GET', 'POST'))
 def index():
@@ -20,7 +22,11 @@ def index():
     files = glob.glob("./ConfigPacks/*.json")
     if request.method == 'POST':
         if "solution" in request.form:
-            json = find_result()
+            path_input = request.form['datasetName']
+            f = open("temp.txt", "w")
+            f.write(path_input)
+            f.close()
+            json = find_result(path_input)
             if json['Facility']['w'] > json['Facility']['h']:
                 k = 800 / json['Facility']['w']
             else:
@@ -31,24 +37,35 @@ def index():
             generateRandomConfigs(request.form['ConfigurationPackName'],
                                   request.form['NumberOfMachines'],
                                   request.form['MaxNumConf'])
-
             files = glob.glob("./ConfigPacks/*.json")
-        elif "test" in request.form:
-            return render_template('test.html')
-
-
+        elif "show_parameters" in request.form:
+            f = open("temp.txt", "r")
+            path_input = f.read()
+            f.close()
+            json = find_result(path_input)
+            if json['Facility']['w'] > json['Facility']['h']:
+                k = 800 / json['Facility']['w']
+            else:
+                k = 500 / json['Facility']['h']
+            if request.form['show_parameters'] == "show":
+                par_vis = True
+            else:
+                par_vis = False
+            return render_template('result.html', dataset=json, randint=randint, k=k, par_vis=par_vis)
 
     return render_template('index.html', files=files)
 
-def find_result() -> dict:
-    path_input = request.form['datasetName']
-    path_output = "./ResultPacks/" + request.form['datasetName'][14:]
+
+def find_result(path_input: str) -> dict:
+    # path_output = "./ResultPacks/" + request.form['datasetName'][14:]
+    path_output = "./ResultPacks/" + path_input[14:]
     configs_data = load_machines_from_json(path_input)
     best_facility = optimize(configs_data)
     write_machines_to_json(path_output, best_facility)
 
     json_result = read_json(path_output)  # result
     return json_result
+
 
 if __name__ == '__main__':
     app.run()
